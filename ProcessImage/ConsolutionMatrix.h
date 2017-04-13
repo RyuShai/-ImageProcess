237bd5ae286eeee08aa6b41cfc9c4ecd57056420
ryuShai <ryu.shai112@gmail.com>
2017-04-13 09:35:15 +0700

    original project

---
+++ ProcessImage/ConsolutionMatrix.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ConsolutionMatrix.h
+ * Author: ryu
+ *
+ * Created on April 13, 2017, 9:33 AM
+ */
+
+#ifndef CONSOLUTIONMATRIX_H
+#define CONSOLUTIONMATRIX_H
+
+class ConsolutionMatrix {
+public:
+    ConsolutionMatrix();
+    ConsolutionMatrix(const ConsolutionMatrix& orig);
+    virtual ~ConsolutionMatrix();
+private:
+
+};
+
+#endif /* CONSOLUTIONMATRIX_H */
+
